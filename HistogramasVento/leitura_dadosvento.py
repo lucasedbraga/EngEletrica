aa2efc31df4cc7b5e92a3d4318f9d14e7f4096ec
lucasedbraga <braga.lucas@engenharia.ufjf.br>
2021-01-21 16:35:37 -0300

    Distribuições de Rayleigh e Weibull

--- HistogramasVento/leitura_dadosvento.py
+++ HistogramasVento/leitura_dadosvento.py
@@ -1,5 +1,5 @@
 #######################################
-# Atividade 01 e 02 -  ENE103 2020/3  #
+# Atividade 03 e 04 -  ENE103 2020/3  #
 # Lucas Eduardo Silva Braga 201570023 #
 #######################################
 
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import math
 from matplotlib import pyplot as plt
+import statistics
 
 ############################################################################################################
 
@@ -102,8 +103,16 @@ for ano in range(len(anos)):
     #Agrupando os Dados do Ano
     dados_velmed_ano.append(dados_velmed)
 
+
 #Peparando dados para serem plotados
 dados_velmed_ano = pd.DataFrame(dados_velmed_ano)
+velmed_ano = []
+desvpad_ano = []
+for ano in range(len(dados_velmed_ano)):
+    velmed = dados_velmed_ano.iloc[ano,:].mean()
+    desvpad = statistics.stdev(dados_velmed_ano.iloc[ano,:])
+    desvpad_ano.append(desvpad)
+    velmed_ano.append(velmed)
 x = np.array(meses).flatten()
 dados_velmed_ano = np.array(dados_velmed_ano)
 legenda_ano = anos.astype(str)
@@ -112,53 +121,80 @@ legenda_ano = anos.astype(str)
 
 # 4- HISTOGRAMA COM A DISTRIBUIÇÃO ANUAL DA VELOCIDADE DO VENTO
 
+
+
+dados_hist = []
+for ano in range(len(anos)):
+    dados_hist_ano = []
+    #Selecionando conjunto de dados com Ano
+    dado_hist = dados.loc[dados[2] == anos.iloc[ano][2]]
+    dado_hist = dado_hist.iloc[:, 3:]
+    dado_hist = pd.DataFrame(dado_hist)
+    dados_hist.append(dado_hist)
+
+
 #Iniciando o contador de horas/ano
-contador = np.zeros((15))
-#Coletando o número de Horas
-for col in range(3, len(dados.columns)):
-    for lin in range(len(dados)):
-        velocidade = dados.iloc[lin][col]
+contador_hist = []
+passo = 0.5
+eixo_x = np.arange(0.0, 15.0, passo)
 
-        if velocidade < 0.5:
-            contador[0]+= 1
-        if 0.5 < velocidade < 1.5:
-            contador[1] += 1
-        if 1.5 < velocidade < 2.5:
-            contador[2] += 1
-        if 2.5 < velocidade < 3.5:
-            contador[3] += 1
-        if 3.5 < velocidade < 4.5:
-            contador[4] += 1
-        if 4.5 < velocidade < 5.5:
-            contador[5] += 1
-        if 5.5 < velocidade < 6.5:
-            contador[6] += 1
-        if 6.5 < velocidade < 7.5:
-            contador[7] += 1
-        if 7.5 < velocidade < 8.5:
-            contador[8] += 1
-        if 8.5 < velocidade < 9.5:
-            contador[9] += 1
-        if 9.5 < velocidade < 10.5:
-            contador[10] += 1
-        if 10.5 < velocidade < 11.5:
-            contador[11] += 1
-        if 11.5 < velocidade < 12.5:
-            contador[12] += 1
-        if 12.5 < velocidade < 13.5:
-            contador[13] += 1
-        if 13.5 < velocidade < 14.5:
-            contador[14] += 1
-        if 14.5 < velocidade < 15:
-            contador[15] += 1
+for ano in range(len(dados_hist)):
+    contador = np.zeros((30))
+    #Coletando o número de Horas
+    for col in range(3, len(dados_hist[ano].columns)):
+        for lin in range(len(dados_hist[ano])):
+            for pos in range(len(contador)):
+                velocidade = dados_hist[ano].iloc[lin][col]
+                if int((0.5*(pos-passo)).__abs__()) < velocidade <= 0.5*(pos +0.5) :
+                    contador[pos] += 1
+                    #Dividiando o número de horas por ano
+    contador_hist.append(contador)
+contador_hist = pd.DataFrame(contador_hist)
 
-#Dividiando o número de horas por ano
-contador = contador/(len(anos))
 
-# PlOT
+
+# Distribuição de Rayleigh
+c_list = []
+for ano in range(len(velmed_ano)):
+    c = (2/math.sqrt(math.pi))*velmed_ano[ano]
+    c_list.append(c)
+
+
+f_rayleigh = []
+passo_dist = 0.1
+eixo_x_rey = np.arange(0.0, 15.0, passo_dist)
+for ano in range(len(c_list)):
+    f_ano = []
+    for v in range(0,150):
+        v = passo_dist*v
+        rayleigh = ((2*v)/((c_list[ano])**2))*(math.e**(-(v/c_list[ano])**2))
+        f_ano.append(rayleigh)
+    f_rayleigh.append(f_ano)
+
+
+
+# Distribuição de Weibull
+
+f_weibull = []
+
+eixo_x_weib = np.arange(0.0, 15.0, passo_dist)
+for ano in range(len(c_list)):
+    f_ano = []
+    for v in range(0,150):
+        v = passo_dist*v
+        k = (desvpad_ano[ano]/velmed_ano[ano])**(-1.086)
+        c = (velmed_ano[ano])/(math.gamma(1+(1/k)))
+        weibull = (k/c) * ((v/c)**(k-1)) * (math.e**(-((v/c)**k)))
+        f_ano.append(weibull)
+
+    f_weibull.append(f_ano)
+
+
+
+
+# PlOT - Todos os Meses
 plt.figure()
-plt.suptitle('HISTOGRAMAS - DADOS DE VENTO - MOSSORÓ (RN)')
-plt.subplot(2, 1, 1)
+plt.suptitle('DADOS DE VENTO - MOSSORÓ (RN) : Comparação entre os Anos')
 plt.title('Velocidade Média Mensal')
 plt.plot(x, dados_velmed_ano[0], color='red', label=legenda_ano.iloc[0][2])
 plt.plot(x, dados_velmed_ano[1], color='blue', label=legenda_ano.iloc[1][2])
@@ -169,17 +205,50 @@ plt.plot(x, dados_velmed_ano[5], color='#00FF00', label=legenda_ano.iloc[5][2])
 plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
 plt.xlabel('Meses')  # Legenda Eixo X
 plt.ylabel('Velocidade Média (m/s)')  # Legenda Eixo Y
-plt.subplots_adjust(right=0.85, left=0.1)
-
-plt.subplot(2, 1, 2)
-plt.title('Distribuição Anual da Velocidade do Vento')
-eixo_x = list(range(0,15))
-plt.bar(eixo_x, contador, color='blue', label= 'Horas/Ano')
-plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
-plt.xlabel('Velocidade (m/s)')  # Legenda Eixo X
-plt.ylabel('Horas/Ano')  # Legenda Eixo Y
-plt.subplots_adjust(hspace=0.4, right=0.85, left=0.1)
 plt.show()
+
+
+for ano in range(len(anos)):
+    # PlOT - Detalhado
+    plt.suptitle(f'HISTOGRAMAS DE VENTO DETALHADOS - MOSSORÓ (RN) - ANO : {legenda_ano.iloc[ano][2]}')
+    plt.subplot(2, 2, 1)
+    plt.title('Velocidade Média Mensal')
+    plt.plot(x, dados_velmed_ano[ano], color='red', label=legenda_ano.iloc[ano][2])
+    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
+    plt.xlabel('Meses')  # Legenda Eixo X
+    plt.ylabel('Velocidade Média (m/s)')  # Legenda Eixo Y
+    plt.subplots_adjust(hspace=0.5, right=0.95, left=0.04)
+
+    ax = plt.subplot(2, 2, 2)
+    plt.title('Distribuição Anual da Velocidade do Vento')
+    histogramas = ax.bar(eixo_x, contador_hist.iloc[ano,:], width=(passo), color='blue', label= legenda_ano.iloc[ano][2])
+    for bar in histogramas:
+        bar.set_edgecolor("black")
+        bar.set_linewidth(0.5)
+    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
+    plt.xlabel('Velocidade (m/s)')  # Legenda Eixo X
+    plt.ylabel('Horas/Ano')  # Legenda Eixo Y
+    plt.subplots_adjust(hspace=0.5, right=0.98, left=0.04)
+
+
+    plt.subplot(2, 2, 3)
+    plt.title(f'Distribuição de Rayleigh')
+    plt.bar(eixo_x_rey, f_rayleigh[ano], width=(passo_dist/2), color='green', label=legenda_ano.iloc[ano][2])
+    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
+    plt.xlabel('Velocidade (m/s)')  # Legenda Eixo X
+    plt.ylabel('Probabilidade')  # Legenda Eixo Y
+    plt.subplots_adjust(hspace=0.5, right=0.95, left=0.04)
+
+
+    plt.subplot(2, 2, 4)
+    plt.title(f'Distribuição de Weibull')
+    plt.bar(eixo_x_weib, f_weibull[ano], width=(passo_dist / 2), color='#00BFFF', label=legenda_ano.iloc[ano][2])
+    plt.legend(bbox_to_anchor=(1.0, 1.0), loc='best', borderaxespad=0.1)
+    plt.xlabel('Velocidade (m/s)')  # Legenda Eixo X
+    plt.ylabel('Probabilidade')  # Legenda Eixo Y
+    plt.subplots_adjust(hspace=0.5, right=0.95, left=0.04)
+
+    plt.show()
 
 ##############################################################################################################
 
